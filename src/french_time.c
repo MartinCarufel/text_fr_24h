cff6d457f3f4a847dc639de773ba180aad2214b3
MartinCarufel <maccam6@gmail.com>
2014-11-18 13:17:37 -0500

    First commit

---
+++ src/french_time.c
@@ -0,0 +1,199 @@
+#include "french_time.h"
+
+static char* const STR_HEURE = "heure";
+static char* const STR_DIX_H = "dix";
+static char* const STR_VINGT_H = "vingt";
+static const char* STR_H = " h.";
+
+static char* SEIZEHEURES[] = {
+	"minuit","une","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","midi","treize","quatorze","quinze","seize"};
+static char* DIXHEURES[] = {"sept","huit","neuf","vingt"};
+static char* VINGTHEURES[] = {"une","deux","trois"};
+
+static char* SEIZEMINS[] = {
+	"pile",
+  "une",
+  "deux",
+  "trois",
+  "quatre",
+  "cinq",
+  "six",
+  "sept",
+  "huit",
+  "neuf",
+  "dix",
+  "onze",
+  "douze",
+  "treize",
+  "quatorze",
+  "quinze",
+  "seize"};
+
+static char* DIZAINE[] = {
+	"",
+  "dix",
+  "vingt",
+  "trente",
+  "quarante",
+  "cinquante"};
+
+static char* DIZAINEMINS[] = {
+	"",
+  "et une",
+  "deux",
+  "trois",
+  "quatre",
+  "cinq",
+  "six",
+  "sept",
+  "huit",
+  "neuf"};
+
+
+
+static const char* JOURS[] = {
+	"Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
+
+static const char* MOIS[] = {
+	"Jan.","Fév.","Mars","Avril","Mai","Juin","Juil.","Août","Sep.","Oct.","Nov.","Déc."};
+
+int textwatch_time(char* line1, char* line2, char* line3, char* line4, struct tm * t) {
+
+	int hours = t->tm_hour;
+	int minutes = t->tm_min;
+  
+	int nbLine = 3;
+	strncpy(line1, "",LINE_BUFFER_SIZE -1);
+	strncpy(line2, "",LINE_BUFFER_SIZE -1);
+	strncpy(line3, "",LINE_BUFFER_SIZE -1);
+	strncpy(line4, "",LINE_BUFFER_SIZE -1);	
+	
+  //ne dépasse jamais 23 (retourne à 0)
+  if (hours >= 24) hours = 0;
+  //pour transforer le 24h en 15h
+	//if (hours > 12) hours -= 12;
+  
+  
+  //////////
+  //HEURE
+  //////////
+
+  
+  //////////
+  //MINUTES
+  //////////
+  
+  //nombre de lignes pour afficher les minutes
+  int lignesminutes;
+  
+  //carmina burana
+  //https://www.youtube.com/watch?v=GXFSK0ogeg4
+  char* mina = "";
+  
+  //...
+  char* minb = "";
+  
+  //Jusqu'a 16
+  if (minutes <= 16)
+  {
+    //affiche les 16 premières minutes
+    mina = SEIZEMINS[minutes];
+      
+    int lignesminutes = 1;
+  }
+  //Au dessus de 16 jusqu'a 59
+  else if (minutes > 16  && minutes <= 59) 
+  {
+    int lignesminutes = 2;
+    
+ 
+          mina = DIZAINE[minutes / 10];
+          minb = DIZAINEMINS[minutes % 10];
+    
+  }
+  
+  //////////
+  //AFFICHAGE
+  //////////
+
+  if (hours >= 1 && hours <= 11) //On affiche Heure sur la seconde ligne
+  {
+    strcat(line1, SEIZEHEURES[hours]); //on affiche l'heure (toujours en première ligne)
+    strcat(line2, STR_HEURE);
+    strcat(line3, mina);
+    strcat(line4, minb);
+		if (hours > 1) strcat(line2, "s"); //rajoute un "s" à heure
+  }
+  
+  //si minuit ou midi on affiche pas heure
+  if (hours == 0 || hours == 12)
+  {
+		strcat(line1, SEIZEHEURES[hours]);    
+		strcat(line2, mina);
+    strcat(line3, minb);
+  }
+  if (hours >= 13 && hours <= 16) //On affiche Heure sur la seconde ligne
+  {
+    strcat(line1, SEIZEHEURES[hours]); //on affiche l'heure (toujours en première ligne)
+    strcat(line2, STR_HEURE);
+    strcat(line3, mina);
+    strcat(line4, minb);
+		if (hours > 1) strcat(line2, "s"); //rajoute un "s" à heure
+  }
+
+ if (hours >= 17 && hours <= 19) //On affiche "h." sur la seconde ligne
+  {
+    strcat(line1, STR_DIX_H);
+		strcat(line2, DIXHEURES[hours-17]); //on affiche l'heure (toujours en première ligne)
+    strcat(line2, STR_H);
+    strcat(line3, mina);
+    strcat(line4, minb);
+  }
+
+if (hours == 20) //On affiche "heures" sur la seconde ligne
+  {
+		strcat(line1, DIXHEURES[hours-17]); //on affiche l'heure (toujours en première ligne)
+    strcat(line2, STR_HEURE);
+    strcat(line3, mina);
+    strcat(line4, minb);
+	  if (hours > 1) strcat(line2, "s"); //rajoute un "s" à heure
+	}
+
+if (hours >= 21 && hours <= 23) //On affiche "h." sur la seconde ligne
+  {
+    strcat(line1, STR_VINGT_H);
+		strcat(line2, VINGTHEURES[hours-21]); //on affiche l'heure (toujours en première ligne)
+		strcat(line2, STR_H);
+    strcat(line3, mina);
+    strcat(line4, minb);
+	}
+
+  
+  //Retourne le nombre de lignes
+  //return nbLine;
+  return 4;
+}
+
+void info_lines(char* line1,struct tm * t) {
+	
+	char wday[LINE_BUFFER_SIZE];
+	char mday[LINE_BUFFER_SIZE];
+	char mon[LINE_BUFFER_SIZE];
+	strncpy(line1, "",LINE_BUFFER_SIZE -1);
+	snprintf(wday, LINE_BUFFER_SIZE, "%s", JOURS[t->tm_wday]);
+	snprintf(mday, LINE_BUFFER_SIZE, "%d", t->tm_mday);
+	snprintf(mon, LINE_BUFFER_SIZE, "%s", MOIS[t->tm_mon]);
+	strcat(line1, wday);
+	strcat(line1, " ");
+	strcat(line1, mday);
+	strcat(line1, " ");
+	strcat(line1, mon);
+
+
+}
+
+void majMinute(char * str, struct tm * t){
+	//str[0] = 'a';
+	//strcpy(str, "a");
+	snprintf(str, 3, "%d", t->tm_min);
+}
